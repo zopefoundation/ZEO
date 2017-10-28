924c5b0f1cc59d41a2e867e3c33d53c419fbdeca
Mauro Amico <mauro.amico@gmail.com>
2017-10-28 18:27:41 +0200

    test infos

--- src/ZEO/tests/test_marshal.py
+++ src/ZEO/tests/test_marshal.py
@@ -7,7 +7,10 @@ from ZopeUndo.Prefix import Prefix
 class MarshalTests(unittest.TestCase):
 
     def testServerDecodeZopeUndoFilter(self):
-        # this is an example of Zope's 
+        # this is an example (1) of Zope2's arguments for
+        # undoInfo call. Arguments are encoded by ZEO client
+        # and decoded by server. The operation must be idempotent.
+        # (1) https://github.com/zopefoundation/Zope/blob/2.13/src/App/Undo.py#L111
         args = (0, 20, {'user_name': Prefix('test')})
         # test against repr because Prefix __eq__ operator
         # doesn't compare Prefix with Prefix but only
