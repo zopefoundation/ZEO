5c1335ed44a8a60412de57d997c63f0a528d157e
Jens Vagelpohl <jens@plyp.com>
2022-03-19 12:52:29 +0000

    - emit DeprecationWarning for mtacceptor [ci skip]

--- src/ZEO/StorageServer.py
+++ src/ZEO/StorageServer.py
@@ -50,6 +50,8 @@ from ZODB.utils import oid_repr, p64, u64, z64, Lock, RLock
 
 # BBB mtacceptor is unused and will be removed in ZEO version 6
 if os.environ.get("ZEO_MTACCEPTOR"): # mainly for tests
+    warnings.warn('The mtacceptor module is deprecated and will be removed '
+                  'in ZEO version 6.', DeprecationWarning)
     from .asyncio.mtacceptor import Acceptor
 else:
     from .asyncio.server import Acceptor
