a586ef46e0ec00fdf74ed374b6e8002d0bfa430c
dieter <dieter@handshake.de>
2022-05-21 07:44:15 +0200

    drop Python 3.5 support
    --------
    As @d-maurer says unittest.mock in Python3.5 lacks some functionality
    that we use, e.g. assert_called_once.
    
    Python 3.5 was marked as EOL in 2017, and it stopped receiving security
    updates in 2020. It should be safe to drop support for it.

--- setup.py
+++ setup.py
@@ -46,7 +46,6 @@ classifiers = """
 Intended Audience :: Developers
 License :: OSI Approved :: Zope Public License
 Programming Language :: Python :: 3
-Programming Language :: Python :: 3.5
 Programming Language :: Python :: 3.6
 Programming Language :: Python :: 3.7
 Programming Language :: Python :: 3.8
@@ -163,5 +162,5 @@ setup(name="ZEO",
       zeo-nagios = ZEO.nagios:main
       """,
       include_package_data=True,
-      python_requires='>=3.5.2',
+      python_requires='>=3.6',
       )
