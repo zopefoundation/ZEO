59aabeaa56b910e7d3746108b617bbf916257d10
Michael Howitz <icemac@gmx.net>
2023-11-23 08:10:36 +0100

    Preparing release 6.0.0

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,7 +1,7 @@
 Changelog
 =========
 
-6.0.0 (unreleased)
+6.0.0 (2023-11-23)
 ------------------
 
 - Drop support for Python 2.7, 3.5, 3.6.
--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 
-version = '6.0.0.dev0'
+version = '6.0.0'
 
 install_requires = [
     'ZODB >= 5.1.1',
